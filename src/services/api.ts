24f224bcfa62470cca462b590d08aa4be43877ca
prudentjag <prudentjag@gmail.com>
2026-01-08 03:28:29 +0100

    added env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://potatoes-falling-upcoming-builds.trycloudflare.com";
+export const BASE_URL = import.meta.env.VITE_APP_URL || "http://localhost:8000";
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
   headers: {
